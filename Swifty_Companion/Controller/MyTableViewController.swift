0142cb4ee904a70092f5684e2d509029496a5b95
Kateryna Zakharchuk <mki.just.me@gmail.com>
2018-02-07 00:23:36 +0200

    correcting

--- Swifty_Companion/Controller/MyTableViewController.swift
+++ Swifty_Companion/Controller/MyTableViewController.swift
@@ -8,36 +8,59 @@
 
 import UIKit
 
+fileprivate enum SectionIdentifiers {
+    case profile
+    case skills
+    case projects
+}
+
 class MyTableViewController: UITableViewController {
     
-    var studentInfo: StudentStruct?
-    let sections: [String] = ["Profile", "Skills", "Projects"]
-    let sectionsImages: [UIImage] = [#imageLiteral(resourceName: "profile"), #imageLiteral(resourceName: "skills"), #imageLiteral(resourceName: "project")]
+    var studentInfo: StudentStruct!
+    fileprivate var sections: [SectionIdentifiers] = [.profile, .skills, .projects]
+    private let sectionsImages: [UIImage] = [#imageLiteral(resourceName: "profile"), #imageLiteral(resourceName: "skills"), #imageLiteral(resourceName: "project")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSections()
     }
-    // MARK: - Table view data source
+    
+    private func configureSections() {
+        setupSkills()
+        setupProjects()
+        tableView.reloadData()
+    }
+    
+    private func setupSkills() {
+        if studentInfo.skills.isEmpty {
+            if let index = sections.index(of: .skills) {
+                sections.remove(at: index)
+            }
+        }
+    }
+    
+    private func setupProjects() {
+        if studentInfo.projects.isEmpty {
+            if let index = sections.index(of: .projects) {
+                sections.remove(at: index)
+            }
+        }
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return sections.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
+        let currentSection = sections[section]
+        switch currentSection {
+        case .profile:
             return 1
+        case .skills:
+                return studentInfo.skills.count
+        case .projects:
+                return studentInfo.projects.count
         }
-        else if section == 1 {
-            if let skillsCount = studentInfo?.skills.count {
-                return skillsCount
-            }
-        }
-        else if section == 2 {
-            if let projectsCount = studentInfo?.projects.count {
-                return projectsCount
-            }
-        }
-        return 0
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -49,7 +72,7 @@ class MyTableViewController: UITableViewController {
         view.addSubview(image)
         
         let label = UILabel()
-        label.text = sections[section]
+        label.text = String(describing: sections[section])
         label.frame = CGRect(x: 45, y: 5, width: 100, height: 35)
         view.addSubview(label)
         
@@ -61,83 +84,32 @@ class MyTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
+        let currentSection = sections[indexPath.section]
+        switch currentSection {
+        case .profile:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "InfoCell", for: indexPath) as? InfoTableViewCell {
-                if let studInfo = studentInfo {
-                    cell.setStudentLabels(for: studInfo)
-                }
+                    cell.setStudentLabels(for: studentInfo)
                 return cell
             }
-        }
-        else if indexPath.section == 1 {
+        case .skills:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "SkillsCell", for: indexPath) as? SkillsTableViewCell {
-                if let someSkill = studentInfo?.skills[indexPath.row] {
-                    cell.setStudentSkills(skill: someSkill)
-                }
+                    cell.setStudentSkills(skill: studentInfo.skills[indexPath.row])
                 return cell
             }
-        }
-        else if indexPath.section == 2 {
+        case .projects:
             if let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell", for: indexPath) as? ProjectTableViewCell {
-                if let someProject = studentInfo?.projects[indexPath.row] {
-                    cell.setStudentProject(project: someProject)
-                    if let valid = someProject.validated {
+                    cell.setStudentProject(project: studentInfo.projects[indexPath.row])
+                    if let valid = studentInfo.projects[indexPath.row].validated {
                         if valid == true {
                             cell.contentView.backgroundColor = UIColor(red:0.92, green:0.88, blue:0.95, alpha:1.0)
                         } else {
                             cell.contentView.backgroundColor = UIColor(red:0.96, green:0.96, blue:0.96, alpha:1.0)
                         }
                     }
-                }
                 return cell
             }
         }
         return UITableViewCell()
     }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
+
